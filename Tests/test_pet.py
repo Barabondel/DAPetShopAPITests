ec3fdfedf045b014b952ca1e9b3d15046c3c2d07
barabondel <baron.tort@gmail.com>
2025-10-10 18:42:09 +0300

    Добавил тест "Попытка обновить несуществующего питомца"

--- Tests/test_pet.py
+++ Tests/test_pet.py
@@ -34,7 +34,7 @@ class TestPet:
             assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
 
     @allure.title("Попытка получить несуществующего питомца")
-    def test_get_nonexistent_pet(self):  # ✅ Правильное объявление метода
+    def test_get_nonexistent_pet(self):
         with allure.step("Отправка запроса на получение несуществующего питомца"):
             response = requests.get(url=f"{BASE_URL}/pet/9999")
 
