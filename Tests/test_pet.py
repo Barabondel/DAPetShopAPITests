459c53faa4557f2ac3103c8a7d98201d22378c79
barabondel <baron.tort@gmail.com>
2025-10-13 14:44:59 +0300

    Добавление нового питомца и отправка post запроса на подготовленные данные.

--- Tests/test_pet.py
+++ Tests/test_pet.py
@@ -1,5 +1,7 @@
 import allure
 import requests
+import jsonschema
+from schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
@@ -40,3 +42,31 @@ class TestPet:
 
         with allure.step("Проверка статуса ответа"):
             assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+    @allure.title("Добавление нового питомца")
+    def test_add_pet(self):
+        with allure.step("Подготовка данных для создания питомца"):
+            payload = {
+                "id": 10,
+                "name": "doggi",
+                "category": {
+                    "id": 1,
+                    "name": "Dogs"
+                },
+                "photoUrls": ["string"],
+                "tags": [{"id": 0, "name": "string"}],
+                "status": "available"
+            }
+
+        with allure.step("Отправка запроса на создание питомца"):
+            response = requests.post(url=f"{BASE_URL}/pet", json=payload)
+            response_json = response.json()
+
+        with allure.step("Проверка статуса ответа и валидации JSON-схемы"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+            jsonschema.validate(response_json, PET_SCHEMA)
+
+        with allure.step("Проверка параметров питомца в ответе"):
+            assert response_json['id'] == payload['id'], "id питомца не совпадает с ожидаемым"
+            assert response_json['name'] == payload['name'], "name питомца не совпадает с ожидаемым"
+            assert response_json['status'] == payload['status'], "status питомца не совпадает с ожидаемым"
